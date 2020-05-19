d216100dc6a308101bcae895f2603d944e233238
Michael Hernandez <michael.hernandez27@outlook.com>
2020-05-18 20:40:24 -0400

    edited sql files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Jalapeno Cheddar Bacon Burger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('Hamburger', false);
